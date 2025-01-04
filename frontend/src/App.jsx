bdde2b01d433c298aaddaea0278487a56091ce02
kde1a <mrfiadonkor@gmail.com>
2025-01-04 10:37:32 -0600

    react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import MovieCard from './components/MovieCard'
 function App() {
   return (
     <>
-      <MovieCard movie ={{title: "Tims Film", release_date:"2023", }} />
+      <MovieCard movie ={{title: "Delas Film", release_date:"2014"}} />
     </>
   );
 }
