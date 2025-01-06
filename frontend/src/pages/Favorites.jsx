c5858211acdd08d559c6cb93df9c0959c346bb9e
kde1a <mrfiadonkor@gmail.com>
2025-01-06 09:44:26 -0500

    react

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,4 +1,23 @@
-function Favorite() {
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+
+function Favorites() {
+    const {favorites} = useMovieContext();
+
+    if(favorites) {
+        return (
+            <div className="favorites"> 
+                <h2>Your Favorite Movies</h2>
+                <div className="movies-grid">
+                    {favorites.map((movie) => ( <MovieCard movie={movie} key={movie.id} />))}
+                </div>
+            </div>
+        ) 
+            
+    }
+
     return (
         <div className="favorite-empty">
             <h2>No Favorite Movies Yet</h2>
@@ -7,4 +26,4 @@ function Favorite() {
     )
 }
 
-export default Favorite
+export default Favorites
---
+++ frontend/src/services/api.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const API_KEY = "d9129d719f6b967089c6efd7fbb42073";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
